9c32989ac6b1fddc0ad6cc0e8dca90f2bd91e67f
Krayeu U <kraevroot@gmail.com>
2019-07-29 01:37:12 +0300

    add bucket-fill method and executor class

--- task.rb
+++ task.rb
@@ -1,3 +1,6 @@
+require "thread"
+require 'pry'
+
 class Canvas
   def initialize(width, height)
     @width = width
@@ -13,18 +16,16 @@ class Canvas
     canvas
   end
 
-  def to_s
-    # write to file
-    puts "-" * (@width + 2)
-    puts @canvas.map { |line| "|" + line.join("") + "|" }
-    puts "-" * (@width + 2)
+  def write_file(file_path)
+    File.open(file_path, "a") do |line|
+      line.puts "-" * (@width + 2)
+      line.puts @canvas.map { |line| "|" + line.join("") + "|" }
+      line.puts "-" * (@width + 2)
+      line.puts "\n"
+    end
   end
 
   def line(x1, y1, x2, y2)
-    # if points the same
-    # if direction positive
-    # if direction negative
-    # TODO decrease user input 1 = 0, 2 = 1
     x1 -= 1; x2 -= 1; y1 -= 1; y2 -= 1
     cur_x = x1
     cur_y = y1
@@ -32,17 +33,9 @@ class Canvas
       @canvas[cur_y][cur_x] = 'x'
       break if cur_x == x2 && cur_y == y2
       if cur_x == x2
-        if cur_y > y2
-          cur_y -= 1
-        else
-          cur_y += 1
-        end
+        cur_y > y2 ? cur_y -= 1 : cur_y += 1
       else
-        if cur_x > x2
-          cur_x -= 1
-        else
-          cur_x += 1
-        end
+        cur_x > x2 ? cur_x -= 1 : cur_x += 1
       end
     end
   end
@@ -53,14 +46,68 @@ class Canvas
     line(x2, y2, x1, y2)
     line(x2, y2, x2, y1)
   end
+
+  def cell_neighbours(x, y)
+    neighbours = []
+    possible_neighbours = [ [x+1, y], [x, y+1], [x+1, y+1], [x-1, y], [x, y-1], [x-1, y-1], [x-1, y+1], [x+1, y-1] ]
+
+    possible_neighbours.each do |x, y|
+      next if !@canvas[y] || !@canvas[y][x] || x < 0 || y < 0
+      neighbours << [x, y]
+    end
+
+    return neighbours
+  end
+
+  def bucket_fill(x, y, replace_color='z')
+    # TODO add user input checks
+    x -= 1; y -= 1
+    node_color = @canvas[y][x]
+    queue = Queue.new
+    @canvas[y][x] = replace_color
+    queue.enq([x, y])
+    while !queue.empty?
+      candidate_x, candidate_y = queue.deq
+      cell_neighbours(candidate_x, candidate_y).each do |neighbour_x, neighbour_y|
+        if node_color == @canvas[neighbour_y][neighbour_x]
+          @canvas[neighbour_y][neighbour_x] = replace_color
+          queue.enq([neighbour_x, neighbour_y])
+        end
+      end
+    end
+  end
 end
 
-c = Canvas.new(20, 4)
-puts c
+class Executor
+  def initialize()
+    @input = ARGV[0]
+    @canvas_instance = nil
+    @output = File.join(File.expand_path(File.dirname(__FILE__)), 'output.txt')
+  end
 
-c.line(1, 2, 6, 2)
+  def process_file
+    File.open(@input).each do |line|
+      case line
+      when /^C\s(\d+)\s(\d+)$/
+        @canvas_instance = Canvas.new($1.to_i, $2.to_i)
+        @canvas_instance.write_file(@output)
+      when /^L\s(\d+)\s(\d+)\s(\d+)\s(\d+)$/
+        @canvas_instance.line($1.to_i, $2.to_i, $3.to_i, $4.to_i)
+        @canvas_instance.write_file(@output)
+      when /^R\s(\d+)\s(\d+)\s(\d+)\s(\d+)$/
+        @canvas_instance.rectangle($1.to_i, $2.to_i, $3.to_i, $4.to_i)
+        @canvas_instance.write_file(@output)
+      when /^B\s(\d+)\s(\d+)\s(.)$/
+        @canvas_instance.bucket_fill($1.to_i, $2.to_i, $3.to_i)
+        @canvas_instance.write_file(@output)
+      else
+        puts "Unrecognized command: #{line}"
+      end
+    end
+  end
 
-c.line(6, 3, 6, 4)
+end
 
-c.rectangle(16, 1, 20, 3)
-puts c
+
+executor = Executor.new
+executor.process_file
